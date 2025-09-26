70e13af27d0ab7d5d281332291367cfff64464d7
Ernesto SITOE <nelitositoe@gmail.com>
2025-09-26 13:35:05 +0200

    Fixed the ESB table displaying empty rows

--- src/main/java/com/example/spa/model/EsbRow.java
+++ src/main/java/com/example/spa/model/EsbRow.java
@@ -1,21 +1,104 @@
 package com.example.spa.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class EsbRow {
-  private String transactionID;
-  private String candidateID;
-  private String appID;
-  private String reqClient;
-  private String providerName;
-  private String respCode;
-  private String respMessage;
-  private String globalStartTime;
-  private String globalEndTime;
-  public String getTransactionID(){return transactionID;} public void setTransactionID(String v){this.transactionID=v;}
-  public String getCandidateID(){return candidateID;} public void setCandidateID(String v){this.candidateID=v;}
-  public String getAppID(){return appID;} public void setAppID(String v){this.appID=v;}
-  public String getReqClient(){return reqClient;} public void setReqClient(String v){this.reqClient=v;}
-  public String getProviderName(){return providerName;} public void setProviderName(String v){this.providerName=v;}
-  public String getRespCode(){return respCode;} public void setRespCode(String v){this.respCode=v;}
-  public String getRespMessage(){return respMessage;} public void setRespMessage(String v){this.respMessage=v;}
-  public String getGlobalStartTime(){return globalStartTime;} public void setGlobalStartTime(String v){this.globalStartTime=v;}
-  public String getGlobalEndTime(){return globalEndTime;} public void setGlobalEndTime(String v){this.globalEndTime=v;}
+	@JsonProperty("TransactionID")
+	private String transactionId;
+
+	@JsonProperty("CandidateID")
+	private String candidateId;
+
+	@JsonProperty("AppID")
+	private String appId;
+
+	@JsonProperty("ReqClient")
+	private String reqClient;
+
+	@JsonProperty("ProviderName")
+	private String providerName;
+
+	@JsonProperty("RespCode")
+	private String respCode;
+
+	@JsonProperty("RespMessage")
+	private String respMessage;
+
+	@JsonProperty("GlobalStartTime")
+	private String globalStartTime;
+
+	@JsonProperty("GlobalEndTime")
+	private String globalEndTime;
+
+	public String getTransactionID() {
+		return transactionId;
+	}
+
+	public void setTransactionID(String v) {
+		this.transactionId = v;
+	}
+
+	public String getCandidateID() {
+		return candidateId;
+	}
+
+	public void setCandidateID(String v) {
+		this.candidateId = v;
+	}
+
+	public String getAppID() {
+		return appId;
+	}
+
+	public void setAppID(String v) {
+		this.appId = v;
+	}
+
+	public String getReqClient() {
+		return reqClient;
+	}
+
+	public void setReqClient(String v) {
+		this.reqClient = v;
+	}
+
+	public String getProviderName() {
+		return providerName;
+	}
+
+	public void setProviderName(String v) {
+		this.providerName = v;
+	}
+
+	public String getRespCode() {
+		return respCode;
+	}
+
+	public void setRespCode(String v) {
+		this.respCode = v;
+	}
+
+	public String getRespMessage() {
+		return respMessage;
+	}
+
+	public void setRespMessage(String v) {
+		this.respMessage = v;
+	}
+
+	public String getGlobalStartTime() {
+		return globalStartTime;
+	}
+
+	public void setGlobalStartTime(String v) {
+		this.globalStartTime = v;
+	}
+
+	public String getGlobalEndTime() {
+		return globalEndTime;
+	}
+
+	public void setGlobalEndTime(String v) {
+		this.globalEndTime = v;
+	}
 }
