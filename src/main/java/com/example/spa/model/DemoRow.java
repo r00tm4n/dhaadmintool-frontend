feb711c64b4aa1bec6e403a8acf4fd7d6d9bbdff
Ernesto SITOE <nelitositoe@gmail.com>
2025-09-29 13:06:38 +0200

    Alpha Release

--- src/main/java/com/example/spa/model/DemoRow.java
+++ src/main/java/com/example/spa/model/DemoRow.java
@@ -1,9 +1,101 @@
 package com.example.spa.model;
+
 public class DemoRow {
-  private String type;
-  private String idnOrPcn;
-  private String formNumber;
-  public String getType(){return type;} public void setType(String v){this.type=v;}
-  public String getIdnOrPcn(){return idnOrPcn;} public void setIdnOrPcn(String v){this.idnOrPcn=v;}
-  public String getFormNumber(){return formNumber;} public void setFormNumber(String v){this.formNumber=v;}
+    private int type;
+    private String idnOrPcn;
+    private String tcn;
+    private String registrationId;
+    private String batchNumber;
+    private String formNumber;
+    private String inactive;
+    private String blocked;
+    private String dead;
+    private String dateCreated;
+    private String personCreateDate;
+    private String dateLastModified;
+
+    // Getters and setters
+    public int getType() {
+        return type;
+    }
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getIdnOrPcn() {
+        return idnOrPcn;
+    }
+    public void setIdnOrPcn(String idnOrPcn) {
+        this.idnOrPcn = idnOrPcn;
+    }
+
+    public String getTcn() {
+        return tcn;
+    }
+    public void setTcn(String tcn) {
+        this.tcn = tcn;
+    }
+
+    public String getRegistrationId() {
+        return registrationId;
+    }
+    public void setRegistrationId(String registrationId) {
+        this.registrationId = registrationId;
+    }
+
+    public String getBatchNumber() {
+        return batchNumber;
+    }
+    public void setBatchNumber(String batchNumber) {
+        this.batchNumber = batchNumber;
+    }
+
+    public String getFormNumber() {
+        return formNumber;
+    }
+    public void setFormNumber(String formNumber) {
+        this.formNumber = formNumber;
+    }
+
+    public String getInactive() {
+        return inactive;
+    }
+    public void setInactive(String inactive) {
+        this.inactive = inactive;
+    }
+
+    public String getBlocked() {
+        return blocked;
+    }
+    public void setBlocked(String blocked) {
+        this.blocked = blocked;
+    }
+
+    public String getDead() {
+        return dead;
+    }
+    public void setDead(String dead) {
+        this.dead = dead;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getPersonCreateDate() {
+        return personCreateDate;
+    }
+    public void setPersonCreateDate(String personCreateDate) {
+        this.personCreateDate = personCreateDate;
+    }
+
+    public String getDateLastModified() {
+        return dateLastModified;
+    }
+    public void setDateLastModified(String dateLastModified) {
+        this.dateLastModified = dateLastModified;
+    }
 }
